fc9bb5bf419f8e9485c8dc97e049980020275ef7
Miguel <lucariodrago@gmail.com>
2021-11-22 14:59:13 -0500

    restaurar contrasena

--- Peluqueate/CambiarContrasena.xaml.cs
+++ Peluqueate/CambiarContrasena.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,10 +13,39 @@ namespace Peluqueate
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CambiarContrasena : ContentPage
     {
-        public CambiarContrasena()
+        private const string url = "http://192.168.194.128:82/api/";
+        private int _pk;
+        public CambiarContrasena(int pk)
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
+            _pk = pk;
+        }
+
+        private async void BtnRContrasena_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtContrasena1.Text.Equals(txtContrasena2.Text))
+                {
+                    WebClient web = new WebClient();
+                    var request = new System.Collections.Specialized.NameValueCollection();
+                    request.Add("user_contrasena", txtContrasena1.Text);
+                    web.UploadValues(url + "usuarios/contrasena/"+_pk, "PUT", request);
+                    await DisplayAlert("Correcto", "Guardado correctamente", "Ok");
+                    await Navigation.PushAsync(new LoginUsuarios());
+                }
+                else
+                {
+                    await DisplayAlert("Error", "Contraseña no son iguales", "Ok");
+                }
+            }
+            catch(Exception ex){
+                    await DisplayAlert("Error", ex.Message, "Ok");
+
+            }
+
+
         }
     }
 }
---
+++ Peluqueate/Models/Pregunta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Peluqueate.Models
+{
+    class Pregunta
+    {
+        public int id_preguntas { get; set; }
+        public string PRE_PREGUNTAS { get; set; }
+    }
+}
